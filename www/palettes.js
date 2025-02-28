d2ba7734105182415a90480106bbcb247ea79ffc
Tatyana <xbutt001@studenti.czu.cz>
2025-02-28 22:59:22 +0100

    updated version

---
+++ www/palettes.js
@@ -0,0 +1,23 @@
+setTimeout(() => {
+    Shiny.addCustomMessageHandler("plotColors", function(colors) {
+        console.log("Received colors from R:", colors);
+
+        const dygraphsElement = document.getElementById("dygraph");
+        if (dygraphsElement) {
+            const dygraphsWidget = window.HTMLWidgets.getInstance(dygraphsElement);
+            if (dygraphsWidget) {
+                const dygraphObject = dygraphsWidget.dygraph;
+                if (dygraphObject) {
+                    console.log("Updating Dygraph colors:", colors);
+                    dygraphObject.updateOptions({ colors: colors });
+                } else {
+                    console.log("Error: Dygraph object not found.");
+                }
+            } else {
+                console.log("Error: Dygraphs widget not found.");
+            }
+        } else {
+            console.log("Error: Dygraph element not found.");
+        }
+    });
+}, 1000); 
